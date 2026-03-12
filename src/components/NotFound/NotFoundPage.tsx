4b3ccdf7d48d435f732680f970bd0ee817d2d1b1
Danyil Pod <daniilpodlesovskiy@gmail.com>
2026-03-12 20:25:14 +0100

    fix

--- src/components/NotFound/NotFoundPage.tsx
+++ src/components/NotFound/NotFoundPage.tsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 
 export const NotFoundPage = () => {
   return (
-    <div className="bg-[black] flex justify-center m-auto flex-col">
+    <body className="bg-[black] flex justify-center m-auto flex-col mt-[200px]">
       <h1 className="text-[50px] text-[white]">404</h1>
       <br />
       <p className="text-[40px] text-[white]">not found</p>
       <p>Return to home page</p> <br />
-      <Link to={"/"}>Home page</Link>
-    </div>
+      <Link to={"/"} className="text-[white]">
+        Home page
+      </Link>
+    </body>
   );
 };
